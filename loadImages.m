d2e47019b251ff1292f6766f05f0069bfbd1b470
dbanyay <banyaydani@gmail.com>
2018-05-18 13:44:08 +0200

    asd

---
+++ loadImages.m
@@ -0,0 +1,19 @@
+function Imf = loadImages()
+% loadImages loads castle model images from folder as 
+% greyscale single values
+
+cntr = 1;
+
+for num = 586:604
+    imageLoc = ['model_castle\8ADT8' num2str(num) '.JPG'];
+    im = im2single(rgb2gray(imread(imageLoc)));
+    if num == 1
+        Imf=zeros(size(im,1),size(im,2),19);
+    end
+    Imf(:,:,cntr)=im;
+    cntr = cntr + 1;
+
+end
+
+end
+
