d3444f215a1bb5693e94b24e03916bb43c510369
dbanyay <banyaydani@gmail.com>
2018-05-18 13:46:07 +0200

    starting point

--- main.m
+++ main.m
@@ -1,1 +1,33 @@
-a = 1;
+run('D:\TU Delft no sync\SIFT\vlfeat-0.9.21/toolbox/vl_setup')
+
+
+%% Load images
+
+%Imf = loadImages(); % Imf is saved to a .mat file to be faster
+
+load('Imf') % load image matrix
+
+
+%% Feature detection and extraction of SIFT points
+
+
+%% Apply normalized 8-point RANSAC and find best matches
+
+
+%% Chaining
+
+
+
+%% Stitching
+
+
+%% Apply bundle adjustment
+
+
+%% Eliminate Affine ambiguity
+
+
+%% 3D model plotting
+
+
+
