4ad6106f58c6b2ebf240d087cb1a2961a64516b9
dbanyay <banyaydani@gmail.com>
2018-05-26 18:12:32 +0200

    getting confused

--- createMeasurementMatrix.m
+++ createMeasurementMatrix.m
@@ -12,8 +12,13 @@ function measurementMatrix = createMeasurementMatrix(bestMatches,frames,descs)
     for frame = 2:size(frames,3) -1        
               
         for i = 1:prev_num_points
-            desc1(:,i) = descs(:,bestMatches(i,5,frame),frame); 
-            desc2(:,i) = descs(:,bestMatches(i,6,frame),frame+1); 
+            desc1(:,i) = descs(:,bestMatches(i,5,frame),frame);
+        end
+        
+        cur_num_points = length(nonzeros(bestMatches(:,1,frame)));
+        
+        for j = 1: cur_num_points
+            desc2(:,j) = descs(:,bestMatches(j,6,frame),frame+1);
         end
 
         
@@ -21,21 +26,8 @@ function measurementMatrix = createMeasurementMatrix(bestMatches,frames,descs)
         % look for matches between previous frame and current frame
         
         for i = 1:size(matches,2)    
-            measurementMatrix((frame+1)*2-1:(frame+1)*2,matches(1,i)) = bestMatches(matches(i),3:4,frame)';
-        end
-        
-        
-        desc_prev_frame = desc_cur_frame(:,matches(2,1:size(matches,2)));
-     
-
-        remainingmatches = nummatches - size(matches,2); % number of matches still to be filled  
-        randindexes = randperm(size(frames,2),nummatches);
-% 
-%         for j = 1:nummatches
-%             randmatches(:,j) = matches(:,randindexes(j));
-%         end      
-        measurementMatrix(frame*2-1:frame*2, size(matches,2):nummatches) = 0;
-       
+            measurementMatrix((frame+1)*2-1:(frame+1)*2,matches(1,i)) = bestMatches(matches(2,i),3:4,frame)';
+        end     
     end   
 end
 
