d9a0393f224580a74435dfc9e4b49e73441f2f04
dbanyay <banyaydani@gmail.com>
2018-06-05 22:42:52 +0200

    added last and first frame mach

--- main.m
+++ main.m
@@ -23,11 +23,11 @@ load('frames')
 
 %bestMatches = eightPointRANSAC(Imf,frames,descs);
 
-
+load('bestMatches')
 %% Chaining
-%[measurementMatrix,bestMatches] = createMeasurementMatrix(bestMatches,frames,descs);
+[measurementMatrix,bestMatches] = createMeasurementMatrix(bestMatches,frames,descs);
 
-load('measurementMatrix')
+%load('measurementMatrix')
 %% Stitching
 
 [M,S] = estimate_3D_points(measurementMatrix,Imf);
